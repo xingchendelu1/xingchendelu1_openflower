27ce79bc9f1b6741e3e0580ec2cc166e5e15d8f5
xingc <xingc@SECRETJOJO>
2023-03-09 12:33:07 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xingchendelu1_openflower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, flower in sunday!");
+        }
+    }
+}
